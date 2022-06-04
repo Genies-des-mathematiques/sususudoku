4a8a25167923e845480e149911fc705abda9ecef
Xanonymous <trusaidlin@gmail.com>
2022-06-05 06:50:11 +0800

    Initial Commit

---
+++ sususudoku/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sususudoku
+//
+//  Created by TeU on 2022/6/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sususudoku/sususudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  sususudokuApp.swift
+//  sususudoku
+//
+//  Created by TeU on 2022/6/5.
+//
+
+import SwiftUI
+
+@main
+struct sususudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
