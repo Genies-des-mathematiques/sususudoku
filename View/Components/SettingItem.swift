277b49b05d080cffd719edabaf8f6f0e381e1aad
Xanonymous <trusaidlin@gmail.com>
2022-06-17 03:07:12 +0800

    feat: add setting item view

---
+++ View/Components/SettingItem.swift
@@ -0,0 +1,31 @@
+//
+//  SettingItem.swift
+//  sususudoku
+//
+//  Created by TeU on 2022/6/17.
+//
+
+import SwiftUI
+
+struct SettiingItem: View {
+    let iconName: String
+    let itemName: String
+    let iconColor: Color
+
+    var body: some View {
+        HStack {
+            Image(systemName: iconName)
+                .foregroundColor(iconColor)
+            NavigationLink(itemName) {}
+        }
+    }
+}
+
+#if DEBUG
+struct SettingItem_Previews: PreviewProvider {
+    static var previews: some View {
+        SettiingItem(iconName: "gearshape", itemName: "設定", iconColor: .red)
+            .previewLayout(.sizeThatFits)
+    }
+}
+#endif
