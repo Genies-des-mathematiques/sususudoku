d4211ae94ed5e648b2b97801d4137fcd9802edec
Xanonymous <trusaidlin@gmail.com>
2022-06-17 03:07:45 +0800

    feat: finish setting page nav items

--- View/Pages/SettingSheet.swift
+++ View/Pages/SettingSheet.swift
@@ -11,16 +11,28 @@ struct SettingSheet: View {
     @Environment(\.dismiss) private var _dismiss
 
     var body: some View {
-        VStack {
-            Text("HIHIHI")
+        List {
+            Section {
+                SettiingItem(iconName: "gearshape", itemName: "設定", iconColor: .red)
+                SettiingItem(iconName: "graduationcap", itemName: "遊玩方式", iconColor: .orange)
+            }
+            Section {
+                SettiingItem(iconName: "info.circle", itemName: "關於遊戲", iconColor: .blue)
+                SettiingItem(iconName: "questionmark.circle", itemName: "幫助", iconColor: .green)
+            }
         }
-        .toolbar{
-            ToolbarItem (placement: .cancellationAction) {
-                Button("Cancel") {
+        .listStyle(GroupedListStyle())
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                Text("選項")
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button("完成") {
                     _dismiss()
                 }
             }
         }
+        .background(Color("AppBackground"))
     }
 }
 
