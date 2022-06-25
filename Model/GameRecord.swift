de35b08fec4186bfd8acc6b156cf18e2846e438a
Xanonymous <trusaidlin@gmail.com>
2022-06-25 18:31:19 +0800

    feat: add created time

--- Model/GameRecord.swift
+++ Model/GameRecord.swift
@@ -5,11 +5,13 @@
 //  Created by TeU on 2022/6/25.
 //
 
+import FirebaseFirestore
 import FirebaseFirestoreSwift
 import Foundation
 
 struct GameRecord: Equatable, Identifiable, Codable {
-    @DocumentID var id: String?
+    @DocumentID private(set) var id: String?
+    @ServerTimestamp private(set) var createdAt: Timestamp?
     let name: String
     let gameTimeInSecond: Int
 }
